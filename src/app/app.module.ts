9a971f7f291123704ff45351cc306d45065ab9a4
s426206 <jannow2@st.amu.edu.pl>
2018-02-08 00:29:13 +0100

    rozpoczęta autentykacja, poprawiony wygląd listy firm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FirmaComponent } from './firma/firma.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {RouterModule, Routes} from '@angular/router';
 import {FirmaService} from './services/firma.service';
 import {AdresService} from './services/adres.service';
@@ -15,13 +15,15 @@ import { ListaHaselComponent } from './lista-hasel/lista-hasel.component';
 import {HasloService} from './services/haslo.service';
 import {FormsModule} from '@angular/forms';
 import { UrzadzenieComponent } from './urzadzenie/urzadzenie.component';
-
-const ROUTES: Routes = [
-  { path: '', redirectTo:  'firmy', pathMatch: 'full'},
-  { path: 'firmy', component: ListaFirmComponent },
-  { path: 'urzadzenia/:idFirmy', component: ListaUrzadzenComponent},
-  { path: 'hasla/:idFirmy', component: ListaHaselComponent}
-];
+import { LoginComponent } from './login/login.component';
+import { AlertComponent } from './alert/alert.component';
+import {AuthenticationService} from './services/authentication.service';
+import {AlertService} from './services/alert.service';
+import {AuthGuard} from './guards/index';
+import {UserService} from './services/user.service';
+import {JwtInterceptor} from './helpers/jwt.interceptor';
+import { HomeComponent } from './home/home.component';
+import {routing} from './app.routing';
 
 @NgModule({
   declarations: [
@@ -31,15 +33,30 @@ const ROUTES: Routes = [
     ListaUrzadzenComponent,
     ListaHaselComponent,
     UrzadzenieComponent,
+    LoginComponent,
+    AlertComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(ROUTES)
+    routing
   ],
-  providers: [FirmaService, AdresService, UrzadzenieService, HasloService],
+  providers: [
+    FirmaService,
+    AdresService,
+    UrzadzenieService,
+    HasloService,
+    AuthenticationService,
+    AlertService,
+    AuthGuard,
+    UserService, {
+    provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
